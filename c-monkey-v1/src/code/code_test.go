3fb9cfa5ffb3db67c241532397c3845562bba688
Srivasu-U <suvasista2@gmail.com>
2024-10-09 23:16:07 +0530

    Beginnings of code disassembly

--- c-monkey-v1/src/code/code_test.go
+++ c-monkey-v1/src/code/code_test.go
@@ -9,6 +9,10 @@ func TestMake(t *testing.T) {
 		expected []byte
 	}{
 		{OpConstant, []int{65534}, []byte{byte(OpConstant), 255, 254}},
+		/* It takes two bytes to represent 65534
+		65534 was chosen to actually verify the BigEndian encoding with most significant bit first.
+		65534 evaluates to 0xFF 0xFE which makes it easier to compare
+		*/
 	}
 
 	for _, tt := range tests {
@@ -21,6 +25,58 @@ func TestMake(t *testing.T) {
 		for i, b := range tt.expected {
 			if instruction[i] != tt.expected[i] {
 				t.Errorf("wrong byte at pos %T. want=%d, got=%d", t, b, instruction[i])
+			}
+		}
+	}
+}
+
+func TestInstructionsString(t *testing.T) {
+	instructions := []Instructions{
+		Make(OpConstant, 1),
+		Make(OpConstant, 2),
+		Make(OpConstant, 65535),
+	}
+
+	expected := `0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+`
+
+	concatted := Instructions{} // Once again, flattening [][]byte to []byte
+	for _, ins := range instructions {
+		concatted = append(concatted, ins...)
+	}
+
+	if concatted.String() != expected {
+		t.Errorf("instructions wrongly formatted. \nwant=%q, \ngot=%q", expected, concatted.String())
+	}
+}
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		op        Opcode
+		operands  []int
+		bytesRead int
+	}{
+		{OpConstant, []int{65535}, 2},
+	}
+
+	for _, tt := range tests {
+		instruction := Make(tt.op, tt.operands...)
+
+		def, err := Lookup(byte(tt.op))
+		if err != nil {
+			t.Fatalf("definition not found: %q\n", err)
+		}
+
+		operandsRead, n := ReadOperands(def, instruction[1:])
+		if n != tt.bytesRead {
+			t.Fatalf("n wrong. want=%d, got=%d", tt.bytesRead, n)
+		}
+
+		for i, want := range tt.operands {
+			if operandsRead[i] != want {
+				t.Errorf("operand wrong. want=%d, got=%d", want, operandsRead[i])
 			}
 		}
 	}
