7110ae06da264222fe0babef9d99dbe4ccad7375
Srivasu-U <suvasista2@gmail.com>
2024-10-18 12:41:04 +0530

    Duplicating code into v5

---
+++ c-monkey-v5/src/repl/repl.go
@@ -0,0 +1,68 @@
+package repl
+
+import (
+	"Compiler/c-monkey-v4/src/compiler"
+	"Compiler/c-monkey-v4/src/lexer"
+	"Compiler/c-monkey-v4/src/object"
+	"Compiler/c-monkey-v4/src/parser"
+	"Compiler/c-monkey-v4/src/vm"
+	"bufio"
+	"fmt"
+	"io"
+)
+
+const PROMPT = ">>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	constants := []object.Object{}
+	globals := make([]object.Object, vm.GlobalsSize)
+	symbolTable := compiler.NewSymbolTable()
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
+		}
+
+		comp := compiler.NewWithState(symbolTable, constants)
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
+		}
+
+		code := comp.Bytecode()
+		constants = code.Constants // Update constants after compilation
+
+		machine := vm.NewWithGlobalStore(code, globals)
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+
+		lastPopped := machine.LastPoppedStackElem()
+		io.WriteString(out, lastPopped.Inspect())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
