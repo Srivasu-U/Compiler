b8e0e708287bd4dc2662badf1a7154464fa9724d
Srivasu-U <suvasista2@gmail.com>
2024-11-01 12:14:01 +0530

    Modifiying ReadOperand() and adding tests

--- c-monkey-v6/src/code/code.go
+++ c-monkey-v6/src/code/code.go
@@ -156,6 +156,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 	for i, width := range def.OperandWidths {
 		switch width {
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
 		}
@@ -168,4 +170,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 func ReadUint16(ins Instructions) uint16 {
 	return binary.BigEndian.Uint16(ins)
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
