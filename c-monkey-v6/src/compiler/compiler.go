0a2075e2620181f6428b6ef51ab929f1c2360087
Srivasu-U <suvasista2@gmail.com>
2024-11-09 17:24:39 +0530

    Compiler support for function params

--- c-monkey-v6/src/compiler/compiler.go
+++ c-monkey-v6/src/compiler/compiler.go
@@ -267,7 +267,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 		c.emit(code.OpIndex)
 
 	case *ast.FunctionLiteral:
-		c.enterScope()              // Enter new scope
+		c.enterScope() // Enter new scope
+
+		for _, p := range node.Parameters {
+			c.symbolTable.Define(p.Value)
+		}
+
 		err := c.Compile(node.Body) // Compile function body
 		if err != nil {
 			return err
@@ -298,7 +303,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if err != nil {
 			return err
 		}
-		c.emit(code.OpCall)
+
+		for _, a := range node.Arguments {
+			err := c.Compile(a)
+			if err != nil {
+				return err
+			}
+		}
+		c.emit(code.OpCall, len(node.Arguments))
 	}
 
 	return nil
